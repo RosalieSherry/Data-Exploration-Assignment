df92b7d3d43e24f2bdfb4821f1d477b8ed7c5200
Rosalie Sherry <70994919+RosalieSherry@users.noreply.github.com>
2021-02-20 11:13:28 -0800

    COMBINED THE DATA

--- Sherry_Data_Exploration_Assignment.R
+++ Sherry_Data_Exploration_Assignment.R
@@ -3,5 +3,31 @@
 library(tidyverse)
 library(jtools)
 library(car)
+library(readr)
 
-#Tidy Data
+'''The College Scorecard was released at the start of September 2015. 
+Among colleges that predominantly grant bachelor’s degrees, did it result in 
+more student interest in high-earnings colleges relative to low-earnings ones 
+(as proxied by Google searches for keywords associated with those colleges)?'''
+
+#Data
+Latest_Scorecard <- read_csv("Lab3_Rawdata/Most+Recent+Cohorts+(Scorecard+Elements).csv")
+
+files <- list.files(path = 'Lab3_Rawdata', pattern = 'trends_up_to_')
+print(files)
+
+prepend <- function(fname) {
+  paste('Lab3_Rawdata/', fname, sep = '')
+}
+
+trends <- files %>%
+  map(prepend) %>%
+  map(read_csv) %>%
+  reduce(rbind)
+
+
+#Playing with Data
+Latest_Scorecard <- Latest_Scorecard %>% rename(schname = INSTNM)
+  
+scorecard_all <- merge(x = Latest_Scorecard, y = trends, by = "schname", all.x = TRUE)  
+###filter(PREDDEG == 3) %>% distinct(INSTNM, .keep_all = TRUE)
